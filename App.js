eca0e442f0729edcfa730d66d0c003f151d521cb
vuggeteja <114563536+vuggeteja@users.noreply.github.com>
2022-11-08 13:57:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '29f051dd-d7f0-431d-85a0-446d19dc30d2';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
